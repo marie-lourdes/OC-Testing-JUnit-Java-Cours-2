5372aac20337a3c5d2da30c24d11f0ea66487660
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-08-04 14:19:10 +0200

    ConversionCalculatorTest: add method of test with class nested and annotation @tag, @DisplayName, @Nested

---
+++ src/main/java/com/openclassrooms/testing/ConversionCalculator.java
@@ -0,0 +1,25 @@
+package com.openclassrooms.testing;
+
+public class ConversionCalculator {
+
+	public Double celsiusToFahrenheit(double d) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Double fahrenheitToCelsius(double d) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Double litresToGallons(double d) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Double radiusToAreaOfCircle(double d) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
