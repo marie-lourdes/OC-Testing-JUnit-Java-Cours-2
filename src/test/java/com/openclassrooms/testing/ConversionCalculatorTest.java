32f7e7c383a0f5a2bcee7f619bd628e1fa9db54c
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-08-04 13:48:35 +0200

    create classe test ConversionCalculatorTest

---
+++ src/test/java/com/openclassrooms/testing/ConversionCalculatorTest.java
@@ -0,0 +1,14 @@
+package com.openclassrooms.testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ConversionCalculatorTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
