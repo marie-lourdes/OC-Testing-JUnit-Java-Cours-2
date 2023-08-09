7ce28413ca14ae49a38840f3cc9e04a2cc969c8f
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-08-09 12:37:18 +0200

    BatchCalculatorTest: delete data operations from url and replace by varibale local tu reduce time execution test

--- src/test/java/com/openclassrooms/testing/calcul/domain/BatchCalculatorTest.java
+++ src/test/java/com/openclassrooms/testing/calcul/domain/BatchCalculatorTest.java
@@ -2,11 +2,9 @@ package com.openclassrooms.testing.calcul.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.URISyntaxException;
-import java.net.URL;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -22,12 +20,14 @@ public class BatchCalculatorTest {
 		// GIVEN
 		Stream<String> operations;
 		
-		// Aller chercher la donnée fournie par un collaborateur sur Internet
-		try (BufferedReader br = new BufferedReader(new InputStreamReader(new URL(
+		// Aller chercher la donnée fournie par un collaborateur sur Internet ce qui fait prendre du temps a tester 
+		//il faut itiliser les données dans une variable local du test
+		/*try (BufferedReader br = new BufferedReader(new InputStreamReader(new URL(
 				"https://gist.githubusercontent.com/geoffreyarthaud/7855a0f4322421f164f1701694f4369d/raw/5ad5f559f010c45c3aa9f8d266618ed5d314d5ea/operations.txt")
-						.openStream()))) {
-			operations = br.lines();
-
+						.openStream()))) {*/
+		//operations = br.lines();
+			operations = Arrays.asList("2 + 2","5 x 4","6 + 8","10 x 3").stream();
+			
 			BatchCalculator batchCalculator = new BatchCalculator(new Calculator());
 
 			// WHEN
@@ -35,6 +35,6 @@ public class BatchCalculatorTest {
 
 			// THEN
 			assertThat(resultats).extracting("solution").containsExactly(4, 20, 14, 30);
-		}
+		//}
 	}
 }
