b16b38290e5ce2647d0cc0db725bea6509ef566c
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-08-09 15:28:58 +0200

    correct test with principe FIRST  in method of test  multiplyAndDivide_shouldBeIdentity() and increment +1 multiplicator, divider b to fix error division by zero

--- src/test/java/com/openclassrooms/testing/calcul/domain/CalculatorTest.java
+++ src/test/java/com/openclassrooms/testing/calcul/domain/CalculatorTest.java
@@ -206,7 +206,7 @@ public class CalculatorTest {
 		int b = r.nextInt() % 3;
 		
 		// WHEN on multiplie a par b puis on divise par b
-		int c = calculatorUnderTest.divide(calculatorUnderTest.multiply(a, b), b);
+		int c = calculatorUnderTest.divide(calculatorUnderTest.multiply(a, b+1), b+1);
 		
 		// THEN on ré-obtient a
 		assertThat(c).isEqualTo(a);
