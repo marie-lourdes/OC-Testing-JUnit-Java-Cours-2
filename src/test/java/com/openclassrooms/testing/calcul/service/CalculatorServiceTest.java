308fc8bb2523e45cc8669f29dacb51a06e151e9a
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-08-12 17:07:10 +0200

    refactor: add mock and delete test unecessary in CalcultorServiceTest

--- src/test/java/com/openclassrooms/testing/calcul/service/CalculatorServiceTest.java
+++ src/test/java/com/openclassrooms/testing/calcul/service/CalculatorServiceTest.java
@@ -1,39 +1,44 @@
 package com.openclassrooms.testing.calcul.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.openclassrooms.testing.calcul.domain.Calculator;
 import com.openclassrooms.testing.calcul.domain.model.CalculationModel;
 import com.openclassrooms.testing.calcul.domain.model.CalculationType;
 
+@ExtendWith(MockitoExtension.class)
 public class CalculatorServiceTest {
 
-	Calculator calculator = new Calculator();
-	// Calculator IS CALLED BY CalculatorService
-	CalculatorService classUnderTest = new CalculatorServiceImpl(calculator);
+	@Mock
+	Calculator calculator;
+
+	CalculatorService classUnderTest;
+
+	@BeforeEach
+	public void init() {
+		classUnderTest = new CalculatorServiceImpl(calculator);
+	}
 
 	@Test
-	public void add_returnsTheSum_ofTwoPositiveNumbers() {
+	public void calculate_shouldUseCalculator_forAddition() {
+		// GIVEN
+		when(calculator.add(1, 2)).thenReturn(3);
+
+		// WHEN
 		final int result = classUnderTest.calculate(
 				new CalculationModel(CalculationType.ADDITION, 1, 2)).getSolution();
+
+		// THEN
+		verify(calculator).add(1, 2);
 		assertThat(result).isEqualTo(3);
 	}
 
-	@Test
-	public void add_returnsTheSum_ofTwoNegativeNumbers() {
-		final int result = classUnderTest.calculate(
-				new CalculationModel(CalculationType.ADDITION, -1, 2))
-				.getSolution();
-
-		assertThat(result).isEqualTo(1);
-	}
-
-	@Test
-	public void add_returnsTheSum_ofZeroAndZero() {
-		final int result = classUnderTest.calculate(
-				new CalculationModel(CalculationType.ADDITION, 0, 0)).getSolution();
-		assertThat(result).isEqualTo(0);
-	}
 }
