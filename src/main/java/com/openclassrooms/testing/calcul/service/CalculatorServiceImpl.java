1f5d9131be7254a17f18629aa43b109f23fa0731
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-08-15 13:58:20 +0200

    calculatorServiceImpl: move try / catch for arithmetic ewception and throw illegal argument  exception switch case DIVISION

--- src/main/java/com/openclassrooms/testing/calcul/service/CalculatorServiceImpl.java
+++ src/main/java/com/openclassrooms/testing/calcul/service/CalculatorServiceImpl.java
@@ -15,34 +15,33 @@ public class CalculatorServiceImpl implements CalculatorService {
 	@Override
 	public CalculationModel calculate(CalculationModel calculationModel) {
 		final CalculationType type = calculationModel.getType();
-		try {
-			Integer response = null;
-			switch (type) {
-			case ADDITION:
-				response = calculator.add(calculationModel.getLeftArgument(), calculationModel.getRightArgument());
-				break;
-			case SUBTRACTION:
-				response = calculator.sub(calculationModel.getLeftArgument(), calculationModel.getRightArgument());
-				break;
-			case MULTIPLICATION:
-				response = calculator.multiply(calculationModel.getLeftArgument(), calculationModel.getRightArgument());
-				break;
-			case DIVISION:
+
+		Integer response = null;
+		switch (type) {
+		case ADDITION:
+			response = calculator.add(calculationModel.getLeftArgument(), calculationModel.getRightArgument());
+			break;
+		case SUBTRACTION:
+			response = calculator.sub(calculationModel.getLeftArgument(), calculationModel.getRightArgument());
+			break;
+		case MULTIPLICATION:
+			response = calculator.multiply(calculationModel.getLeftArgument(), calculationModel.getRightArgument());
+			break;
+		case DIVISION:
+			try {
 				response = calculator.divide(calculationModel.getLeftArgument(), calculationModel.getRightArgument());
+			} catch (ArithmeticException e) {
+				e.printStackTrace();
+				throw new IllegalArgumentException("illegal argument");
 
-				break;
-
-			default:
-				throw new UnsupportedOperationException("Unsupported calculations");
 			}
-
-			calculationModel.setSolution(response);
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new IllegalArgumentException("illegal argument");
-
+			break;
+		default:
+			throw new UnsupportedOperationException("Unsupported calculations");
 		}
+
+		calculationModel.setSolution(response);
+
 		return calculationModel;
 	}
 
